d356971f41dc41e43bf0e8e63db88600bbed9f58
jwentao <jin_wentao@qq.com>
2018-12-24 23:55:22 +0800

    addSelectionSort

--- sort.js
+++ sort.js
@@ -7,5 +7,20 @@ function generateRandomArray (n, rangeL, rangeR) {
 }
 let arr = generateRandomArray(100, 50, 1500);
 
-arr.sort((a, b) => a - b);
+// arr.sort((a, b) => a - b);
+// console.log(arr)
+// 选择排序
+function selectionSort(arr, n){
+    for(let i = 0 ; i < n ; i ++){
+        // 寻找[i, n)区间里的最小值
+        let minIndex = i;
+        for( let j = i + 1 ; j < n ; j ++ )
+        if( arr[j] < arr[minIndex] ) minIndex = j;
+        let temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+}
+
+selectionSort(arr, arr.length);
 console.log(arr)
