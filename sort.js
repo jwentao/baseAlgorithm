7960b4a2a67bedb06f274e582155f3b4f7607e11
jwentao <jin_wentao@qq.com>
2019-02-19 23:40:42 +0800

    first commit

--- sort.js
+++ sort.js
@@ -12,7 +12,7 @@ let arr = generateRandomArray(100, 50, 1500);
 // 选择排序
 function selectionSort(arr, n){
     for(let i = 0 ; i < n ; i ++){
-        // 寻找[i, n)区间里的最小值
+        // 寻找[i, n)区间里的最小值 
         let minIndex = i;
         for( let j = i + 1 ; j < n ; j ++ )
         if( arr[j] < arr[minIndex] ) minIndex = j;
